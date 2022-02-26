c2018acf065050e5dcfbbb6ea4133cde95755f56
steveblomeley <steveblomeley@yahoo.co.uk>
2022-02-26 20:02:06 +0000

    Tweeks

--- Program.cs
+++ Program.cs
@@ -2,8 +2,9 @@ public class Program
 {
     public static void Main(string[] _)
     {
-        //Some actual runnable code to illustrate how the (unexpected?) covariance of arrays in C# can bite you.
+        // An example of how the (unexpected?) covariance of arrays in C# can bite you.
         var fileStreams = new FileStream[5];
+        
         DoStuff(fileStreams);
 
         System.Console.WriteLine("Bet we didn't get to here, did we?");
@@ -12,7 +13,7 @@ public class Program
     public static void DoStuff(Stream[] streams)
     {
         //Lets add a memory stream to our array of streams - that's got to be safe, right?
-        if (streams != null) {
+        if (streams?.Length > 0) {
             streams[0] = new MemoryStream();
         }
     }
